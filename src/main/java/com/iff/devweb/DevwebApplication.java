5009aac7f9dd85ec43d50bb8e59d45c0b344c621
BarretoNV <barretonovaes.vilas@gmail.com>
2024-08-19 20:05:33 -0300

    Add spring ao projeto

---
+++ src/main/java/com/iff/devweb/DevwebApplication.java
@@ -0,0 +1,13 @@
+package com.iff.devweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevwebApplication.class, args);
+	}
+
+}
