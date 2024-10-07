3b5647fcfa9a20c0e3b3f26767e98534b83815a2
BarretoNV <barretonovaes.vilas@gmail.com>
2024-10-07 20:51:09 -0300

    - Declararação contrato API   - Inclusão dependências Swaggeer para documentação

--- src/main/java/com/iff/devweb/util/TreinoMapper.java
+++ src/main/java/com/iff/devweb/util/TreinoMapper.java
@@ -1,19 +1,19 @@
-package com.iff.devweb.util;
-
-import com.iff.devweb.dto.TreinoRequest;
-import com.iff.devweb.entity.Treino;
-import org.springframework.stereotype.Component;
-
-@Component
-public class TreinoMapper {
-
-    public TreinoRepresentation toRepresentation(Treino treino) {
-        return new TreinoRepresentation(
-                treino.getNomeTreino(),
-                treino.getDescricao(),
-                treino.getDuracao(),
-                treino.getData(),
-                treino.getNivelDificuldade().name()
-        );
-    }
-}
+//package com.iff.devweb.util;
+//
+//import com.iff.devweb.dto.TreinoRequest;
+//import com.iff.devweb.entity.Treino;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class TreinoMapper {
+//
+//    public TreinoRepresentation toRepresentation(Treino treino) {
+//        return new TreinoRepresentation(
+//                treino.getNomeTreino(),
+//                treino.getDescricao(),
+//                treino.getDuracao(),
+//                treino.getData(),
+//                treino.getNivelDificuldade().name()
+//        );
+//    }
+//}
